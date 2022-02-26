b845dca1dd46d669df2a55cd4de4665f8004bb05
Ugwuododo Jude Obinna <jobinna50@gmail.com>
2022-02-25 18:18:36 -0700

    added somes thing more

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,8 +1,8 @@
 def hello_world(city):
   print(f'Hello world from {city}')
   #lets add some fun thing
-  yourFeeling = input("How are you today? ")
-  print(f"I am {yourFeeling}")
+  yourFeeling = input("How are you doing? ")
+  print(f"You said you are {yourFeeling}")
 
 
 
